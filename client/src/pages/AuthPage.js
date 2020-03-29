9156e148123096174b3f7ec0353fdbd2462ff02b
Kirill <tylerd239@gmail.com>
2020-03-29 10:20:13 +0300

    qwe

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -39,57 +39,62 @@ export const AuthPage = () => {
 
 	console.log("ПРИЁМ!!!")
 
+	// return (
+	// 	<div className="row">
+	// 		<div className="col s6 offset-s3">
+	// 			<h1>Сократи ссылку</h1>
+	// 			 <div className="card red darken-1">
+	// 		        <div className="card-content white-text">
+	// 		        	<span className="card-title">Авторизация</span>
+
+	// 		      		<div>  
+	// 		      		    <div className="input-field">
+ //          						<input
+ //          						 placeholder="Введите Email"
+ //          						 id="email"
+ //          						 type="email"
+ //          						 name="email"
+ //          						 className="yellow-input"
+ //          						 value={form.email}
+ //          						 onChange = {changeHandler}
+ //          						 />
+ //          						<label htmlFor="email">email</label>
+ //       						</div>
+	// 		        	</div>	
+
+	// 		        	<div>  
+	// 		      		    <div className="input-field">
+ //          						<input
+ //          						 placeholder="Введите пароль"
+ //          						 id="password"
+ //          						 type="password"
+ //          						 name="password"
+ //          						 value={form.password}
+ //          						 onChange = {changeHandler}
+ //          						 />
+ //          						<label htmlFor="password">password</label>
+ //       						</div>
+	// 		        	</div>
+
+
+	// 		        </div>
+
+	// 		        <div className="card-action">
+	// 					<button 
+	// 					className="btn yellow darken-4" 
+	// 					style={{marginRight:10}} 
+	// 					disabled={loading}
+	// 					onClick={loginHandler}
+	// 					>Войти</button>
+	// 					<button className="btn grey lighten-1 black-text" onClick={registerHandler} disabled={loading}>Регистрация</button>
+ //      	 			</div>
+ //     			 </div>
+	// 		</div>
+	// 	</div>
+	// )
+
+
 	return (
-		<div className="row">
-			<div className="col s6 offset-s3">
-				<h1>Сократи ссылку</h1>
-				 <div className="card red darken-1">
-			        <div className="card-content white-text">
-			        	<span className="card-title">Авторизация</span>
-
-			      		<div>  
-			      		    <div className="input-field">
-          						<input
-          						 placeholder="Введите Email"
-          						 id="email"
-          						 type="email"
-          						 name="email"
-          						 className="yellow-input"
-          						 value={form.email}
-          						 onChange = {changeHandler}
-          						 />
-          						<label htmlFor="email">email</label>
-       						</div>
-			        	</div>	
-
-			        	<div>  
-			      		    <div className="input-field">
-          						<input
-          						 placeholder="Введите пароль"
-          						 id="password"
-          						 type="password"
-          						 name="password"
-          						 value={form.password}
-          						 onChange = {changeHandler}
-          						 />
-          						<label htmlFor="password">password</label>
-       						</div>
-			        	</div>
-
-
-			        </div>
-
-			        <div className="card-action">
-						<button 
-						className="btn yellow darken-4" 
-						style={{marginRight:10}} 
-						disabled={loading}
-						onClick={loginHandler}
-						>Войти</button>
-						<button className="btn grey lighten-1 black-text" onClick={registerHandler} disabled={loading}>Регистрация</button>
-      	 			</div>
-     			 </div>
-			</div>
-		</div>
-	)
+	<h1>РАБОТАЕТ</h1>
+		)
 }
