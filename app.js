4db19f7c6c60ce3f8544f573d877e22aa5760586
TylerD239 <tylerd239@gmail.com>
2020-03-30 13:59:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ const app = express()
 app.use(express.json({extended:true}))
 
 //test
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// app.use('/test', function (req, res) {
-//   console.log('Request Type:', req.method);
-// })
+app.use('/test', function (req, res) {
+  console.log('Request Type:', req.method);
+})
+
+
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
